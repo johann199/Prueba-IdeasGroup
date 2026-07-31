602065d651210bce5e598aebab8f5c0ef5fd4883
johann199 <johanngonzakez@gmail.com>
2026-07-31 15:28:00 -0500

    Ajuste inicial de la arquitectura Hexagonal

--- PruebaIdeasGroup/obj/Debug/net8.0/PruebaIdeasGroup.AssemblyInfo.cs
+++ PruebaIdeasGroup/obj/Debug/net8.0/PruebaIdeasGroup.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaIdeasGroup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ce2923ecf3ab7946a8e11856be02fc5dacdbe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaIdeasGroup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaIdeasGroup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
